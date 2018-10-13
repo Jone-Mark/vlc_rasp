42d6e6066675da6a4de454af49cd96054793423a
Mengke <35460065+Jone-Mark@users.noreply.github.com>
2018-10-13 22:14:47 +0800

    Initial commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class TOK_IOT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TOK_IOT));
+            this.axVLCPlugin21 = new AxAXVLC.AxVLCPlugin2();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axVLCPlugin21
+            // 
+            this.axVLCPlugin21.Enabled = true;
+            this.axVLCPlugin21.Location = new System.Drawing.Point(12, 12);
+            this.axVLCPlugin21.Name = "axVLCPlugin21";
+            this.axVLCPlugin21.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axVLCPlugin21.OcxState")));
+            this.axVLCPlugin21.Size = new System.Drawing.Size(715, 505);
+            this.axVLCPlugin21.TabIndex = 0;
+            // 
+            // TOK_IOT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 529);
+            this.Controls.Add(this.axVLCPlugin21);
+            this.Name = "TOK_IOT";
+            this.Text = "TOK_IOT";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxAXVLC.AxVLCPlugin2 axVLCPlugin21;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class TOK_IOT : Form
+    {
+        public TOK_IOT()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // this.axVLCPlugin21.playlist.add(@"http://bitdash-a.akamaihd.net/content/sintel/hls/video/250kbit.m3u8");
+            this.axVLCPlugin21.playlist.add(@"rtsp://47.106.209.211:8554/tok.mp4");
+            this.axVLCPlugin21.playlist.play();
+        }
+    }
+}
